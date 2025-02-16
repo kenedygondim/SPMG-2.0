fe77c38f191291df9aa6c915e96cbbb61124206e
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-16 12:53:01 -0300

    feat: services

---
+++ src/main/java/com/project/sp_medical_group/Services/ConvenioService.java
@@ -0,0 +1,43 @@
+package com.project.sp_medical_group.Services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.sp_medical_group.Dto.CriarConvenioDto;
+import com.project.sp_medical_group.Handler.BusinessException;
+import com.project.sp_medical_group.Jpa.Repositories.ConvenioJpaRepository;
+import com.project.sp_medical_group.Models.Convenio;
+import com.project.sp_medical_group.Repositories.ConvenioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ConvenioService implements ConvenioRepository {
+    private final ConvenioJpaRepository convenioJpaRepository;
+
+    @Autowired
+    public ConvenioService(ConvenioJpaRepository convenioJpaRepository) {
+        this.convenioJpaRepository = convenioJpaRepository;
+    }
+
+    @Override
+    public Convenio createConvenio(CriarConvenioDto criarConvenioDto) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Convenio convenio = objectMapper.convertValue(criarConvenioDto, Convenio.class);
+            return convenioJpaRepository.save(convenio);
+        }
+        catch (IllegalArgumentException e) {
+            throw new BusinessException("Argumento inválido para conversão de Dto para Classe: " + e.getMessage());
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new BusinessException("Já existe um convênio cadastrado com esse nome: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Convenio> getAllConvenios() {
+        return convenioJpaRepository.findAll();
+    }
+}
