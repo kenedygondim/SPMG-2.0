38286b8a7526990d7094ba6a66a81cab900f62cd
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-16 12:54:22 -0300

    chg: alteracao no nome do campo roleNome

--- src/main/java/com/project/sp_medical_group/Models/Role.java
+++ src/main/java/com/project/sp_medical_group/Models/Role.java
@@ -1,43 +1,31 @@
 package com.project.sp_medical_group.Models;
 
 import jakarta.persistence.*;
-import lombok.Generated;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Table(name = "tb_roles")
 public class Role {
     public Role () {}
-    public Role (String roleName) {
-         this.roleName = roleName;
+    public Role (String roleNome) {
+         this.roleNome = roleNome;
     }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "role_id")
     private int roleId;
 
-    @Column(name = "nome")
-    private String roleName;
+    @Column(name = "role_nome")
+    private String roleNome;
 
-    public String getRoleName() {
-        return roleName;
+    public String getRoleNome() {
+        return roleNome;
     }
 
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
+    public void setRoleNome(String roleName) {
+        this.roleNome = roleName;
     }
 
     public int getRoleId() {
         return roleId;
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "roleId=" + roleId +
-                ", roleName='" + roleName + '\'' +
-                '}';
     }
 }
