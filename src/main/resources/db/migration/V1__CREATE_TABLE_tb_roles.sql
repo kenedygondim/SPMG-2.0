db8977b54cef316fba6b99e1546cce002b042aaf
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-16 12:51:54 -0300

    chg: restrição UNIQUE para determinados campos

--- src/main/resources/db/migration/V1__CREATE_TABLE_tb_roles.sql
+++ src/main/resources/db/migration/V1__CREATE_TABLE_tb_roles.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tb_roles (
       role_id INT IDENTITY(1,1),
-      nome VARCHAR(20) NOT NULL,
+      role_nome VARCHAR(20) NOT NULL UNIQUE,
       CONSTRAINT pk_tb_roles PRIMARY KEY (role_id)
 )
--- src/main/resources/db/migration/V8__CREATE_TABLE_tb_convenios.sql
+++ src/main/resources/db/migration/V8__CREATE_TABLE_tb_convenios.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tb_convenios (
       convenio_id INT IDENTITY(1,1),
-      convenio_nome VARCHAR(100) NOT NULL
+      convenio_nome VARCHAR(100) NOT NULL UNIQUE,
       CONSTRAINT pk_tb_convenios PRIMARY KEY (convenio_id)
 );
