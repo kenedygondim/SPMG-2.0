135ee4461502f3bbd28fafce506a81a7b5f3ff92
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-20 20:58:12 -0300

    feat: jpa repositories

---
+++ src/main/java/com/project/sp_medical_group/Jpa/Repositories/ClinicaJpaRepository.java
@@ -0,0 +1,10 @@
+package com.project.sp_medical_group.Jpa.Repositories;
+
+import com.project.sp_medical_group.Models.Clinica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClinicaJpaRepository extends JpaRepository<Clinica, String> {
+    Clinica findByCnpj(String cnpj);
+    Clinica findByNomeFantasia(String nomeFantasia);
+    Clinica findByRazaoSocial(String razaoSocial);
+}
---
+++ src/main/java/com/project/sp_medical_group/Jpa/Repositories/EnderecoJpaRepository.java
@@ -0,0 +1,6 @@
+package com.project.sp_medical_group.Jpa.Repositories;
+
+import com.project.sp_medical_group.Models.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoJpaRepository extends JpaRepository<Endereco, Integer> { }
