c3d94e3eaf9c100b13364ce1d226451986c9a02a
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-22 18:44:52 -0300

    feat: services

--- src/main/java/com/project/sp_medical_group/Services/ClinicaService.java
+++ src/main/java/com/project/sp_medical_group/Services/ClinicaService.java
@@ -1,12 +1,10 @@
 package com.project.sp_medical_group.Services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.sp_medical_group.Dto.CriarClinicaComEnderecoDto;
-import com.project.sp_medical_group.Dto.CriarClinicaDto;
+import com.project.sp_medical_group.Dto.CriarClinicaEnderecoDto;
 import com.project.sp_medical_group.Handler.BusinessException;
 import com.project.sp_medical_group.Jpa.Repositories.ClinicaJpaRepository;
 import com.project.sp_medical_group.Models.Clinica;
-import com.project.sp_medical_group.Models.Convenio;
 import com.project.sp_medical_group.Models.Endereco;
 import com.project.sp_medical_group.Repositories.ClinicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,11 +30,11 @@ public class ClinicaService implements ClinicaRepository {
     }
 
     @Override
-    public Clinica createClinica(CriarClinicaComEnderecoDto criarClinicaComEnderecoDto) {
+    public Clinica createClinica(CriarClinicaEnderecoDto criarClinicaEnderecoDto) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            Clinica clinica = objectMapper.convertValue(criarClinicaComEnderecoDto.clinica(), Clinica.class);
-            Endereco endereco = enderecoService.createEndereco(criarClinicaComEnderecoDto.endereco());
+            Clinica clinica = objectMapper.convertValue(criarClinicaEnderecoDto.clinica(), Clinica.class);
+            Endereco endereco = enderecoService.createEndereco(criarClinicaEnderecoDto.endereco());
             clinica.setEndereco(endereco);
             return clinicaJpaRepository.save(clinica);
         }
---
+++ src/main/java/com/project/sp_medical_group/Services/PessoaService.java
@@ -0,0 +1,46 @@
+package com.project.sp_medical_group.Services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.sp_medical_group.Dto.CriarPessoaDto;
+import com.project.sp_medical_group.Handler.BusinessException;
+import com.project.sp_medical_group.Jpa.Repositories.PessoaJpaRepository;
+import com.project.sp_medical_group.Models.Endereco;
+import com.project.sp_medical_group.Models.Pessoa;
+import com.project.sp_medical_group.Repositories.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PessoaService implements PessoaRepository {
+    private final PessoaJpaRepository pessoaJpaRepository;
+
+    @Autowired
+    public PessoaService(PessoaJpaRepository pessoaJpaRepository) {
+        this.pessoaJpaRepository = pessoaJpaRepository;
+    }
+
+    @Override
+    public List<Pessoa> getAllPessoas() {
+        return pessoaJpaRepository.findAll();
+    }
+
+    @Override
+    public Pessoa createPessoa(CriarPessoaDto criarPessoaDto, Endereco endereco) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Pessoa pessoa = objectMapper.convertValue(criarPessoaDto, Pessoa.class);
+            pessoa.setEndereco(endereco);
+            return pessoaJpaRepository.save(pessoa);
+        }
+        catch (IllegalArgumentException e) {
+            throw new BusinessException("Argumento inválido para conversão de Dto para Classe: " + e.getMessage());
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new BusinessException("Já existe uma pessoa cadastrada com esse CPF ou RG: " + e.getMessage());
+        }
+    }
+
+}
