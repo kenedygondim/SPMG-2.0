bd2b20f649a5645d17c3154c165b64eb7f23d87c
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-22 18:42:58 -0300

    feat: jpa repositories

---
+++ src/main/java/com/project/sp_medical_group/Jpa/Repositories/PacienteJpaRepository.java
@@ -0,0 +1,7 @@
+package com.project.sp_medical_group.Jpa.Repositories;
+
+import com.project.sp_medical_group.Models.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteJpaRepository extends JpaRepository<Paciente, String> {
+}
---
+++ src/main/java/com/project/sp_medical_group/Jpa/Repositories/UsuarioJpaRepository.java
@@ -0,0 +1,7 @@
+package com.project.sp_medical_group.Jpa.Repositories;
+
+import com.project.sp_medical_group.Models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioJpaRepository extends JpaRepository<Usuario, Integer> {
+}
