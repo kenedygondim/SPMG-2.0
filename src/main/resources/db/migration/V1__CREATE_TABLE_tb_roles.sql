41926d5a9628677b11ff9097dffa72b6a8c9fb15
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-22 18:38:50 -0300

    del: arquivos deletados

--- src/main/resources/db/migration/V1__CREATE_TABLE_tb_roles.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tb_roles (
-      role_id INT IDENTITY(1,1),
-      role_nome VARCHAR(20) NOT NULL UNIQUE,
-      CONSTRAINT pk_tb_roles PRIMARY KEY (role_id)
-)
--- src/main/resources/db/migration/V3__CREATE_TABLE_tb_fotos_perfil.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tb_fotos_perfil (
-     foto_perfil_id INT IDENTITY(1,1),
-     foto_perfil_url VARCHAR(200),
-     CONSTRAINT pk_tb_fotos_perfil PRIMARY KEY (foto_perfil_id)
-)
