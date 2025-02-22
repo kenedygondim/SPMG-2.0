ba83dd8adce2531125e4bd1f0c95808f2f888274
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-22 18:46:59 -0300

    chg: campo de sexo poderá ser nulo (caso o usuário deseje não informá-lo)

--- src/main/resources/db/migration/V5__CREATE_TABLE_tb_pessoas.sql
+++ src/main/resources/db/migration/V5__CREATE_TABLE_tb_pessoas.sql
@@ -3,7 +3,7 @@ CREATE TABLE tb_pessoas (
     nome_completo VARCHAR(150) NOT NULL,
     dt_nascimento VARCHAR(10) NOT NULL,
     rg VARCHAR(9) NOT NULL UNIQUE,
-    sexo VARCHAR(30) NOT NULL,
+    sexo VARCHAR(30),
     numero_telefone VARCHAR (20) NOT NULL,
     endereco_id INT NOT NULL,
     CONSTRAINT pk_tb_pessoas PRIMARY KEY (cpf),
