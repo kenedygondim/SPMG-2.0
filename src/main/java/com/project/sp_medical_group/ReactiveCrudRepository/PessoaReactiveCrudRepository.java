e0d90987fed8937e234b92023a0722e9daec73a3
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-02-27 21:03:12 -0300

    fix: o @Query não estava retornando o registro criado, causando interrupção inesperada do fluxo de criação de médicos e pacientes

--- src/main/java/com/project/sp_medical_group/ReactiveCrudRepository/PessoaReactiveCrudRepository.java
+++ src/main/java/com/project/sp_medical_group/ReactiveCrudRepository/PessoaReactiveCrudRepository.java
@@ -8,6 +8,12 @@ import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 public interface PessoaReactiveCrudRepository extends ReactiveCrudRepository<Pessoa, String> {
-    @Query("INSERT INTO tb_pessoas (nome_completo, cpf, rg, sexo, numero_telefone, dt_nascimento, endereco_id) VALUES (:nome, :cpf, :rg, :sexo, :telefone, :dataNascimento, :enderecoId)")
+    @Query(
+    """
+    INSERT INTO tb_pessoas (nome_completo, cpf, rg, sexo, numero_telefone, dt_nascimento, endereco_id)
+    OUTPUT INSERTED.*
+    VALUES (:nome, :cpf, :rg, :sexo, :telefone, :dataNascimento, :enderecoId)
+    """
+    ) //OUTPUT INSERTED.* serve para retornar o registro inserido no banco de dados
     Mono<Pessoa> insertPessoa(String nome, String cpf, String rg, String sexo, String telefone, String dataNascimento, Integer enderecoId);
  }
