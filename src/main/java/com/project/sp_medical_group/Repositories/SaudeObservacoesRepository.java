1e514611b7c9e280a5fef5689c4e0bb7ec4290ba
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-04-05 20:54:25 -0300

    feat: repositories

---
+++ src/main/java/com/project/sp_medical_group/Repositories/SaudeObservacoesRepository.java
@@ -0,0 +1,8 @@
+package com.project.sp_medical_group.Repositories;
+
+import com.project.sp_medical_group.Dto.AdicionarSaudeObservacaoDto;
+import com.project.sp_medical_group.Models.SaudeObservacoes;
+
+public interface SaudeObservacoesRepository {
+    public abstract SaudeObservacoes addSaudeObservacao(String pacienteCpf, AdicionarSaudeObservacaoDto adicionarSaudeObservacaoDto);
+}
