3cf7112c74d810f965b235741a9d63c26c588df3
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-05-03 17:04:16 -0300

    rmv: comentário/imports não utilizados

--- src/main/java/com/project/sp_medical_group/Models/AvaliacaoMedico.java
+++ src/main/java/com/project/sp_medical_group/Models/AvaliacaoMedico.java
@@ -8,19 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-//CREATE TABLE tb_avaliacoes_medico (
-//        avaliacao_id INT IDENTITY(1,1),
-//medico_cpf CHAR(11) NOT NULL,
-//paciente_cpf CHAR(11) NOT NULL,
-//numero_estrelas INT NOT NULL,
-//comentario VARCHAR (255) NOT NULL
-//CONSTRAINT pk_tb_avaliacoes_medico PRIMARY KEY (avaliacao_id),
-//CONSTRAINT fk_tb_avaliacoes_medico_tb_medicos FOREIGN KEY (medico_cpf) REFERENCES tb_medicos (cpf),
-//CONSTRAINT fk_tb_avaliacoes_medico_tb_pacientes FOREIGN KEY (paciente_cpf) REFERENCES tb_pacientes (cpf)
-//        );
-
 @Entity
-
 @Table(name = "tb_avaliacoes_medico")
 @Getter
 @Setter
